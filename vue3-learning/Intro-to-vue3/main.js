3a120a57b4e50f3a8d36722137da6edd2fe45f18
jinchenxing <2541226493@qq.com>
2023-03-17 14:58:52 +0800

    计算属性练习

--- vue3-learning/Intro-to-vue3/main.js
+++ vue3-learning/Intro-to-vue3/main.js
@@ -5,15 +5,31 @@ const app = Vue.createApp({
             description: 'A warm fuzzy pair of socks.',
             image: './asserts/images/socks_blue.jpg',
             url: "https://cn.vuejs.org/",
-            inStock: false,
+            inStock: true,
             stock: 10,
+            price: 50,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                { id: 2234, color: 'green', image: './asserts/images/socks_blue.jpg' },
-                { id: 2235, color: 'blue', image: './asserts/images/socks_green.jpg' }
+                { id: 2234, color: 'green', image: './asserts/images/socks_green.jpg', quantity: 50 },
+                { id: 2235, color: 'blue', image: './asserts/images/socks_blue.jpg', quantity: 50 }
             ],
             sizes: ['s', 'm', 'l', 'xl'],
-            cart: 0
+            cart: 0,
+            firstName: '张',
+            lastName: '三'
+        }
+    },
+    computed: {
+        fullName: {
+            get() {
+                return this.firstName + "-" + this.lastName
+            },
+            set(value) {
+                [this.firstName, this.lastName] = value.split('-')
+            }
+        },
+        bill() {
+            return `${this.cart * this.price} $`
         }
     },
     methods: {
