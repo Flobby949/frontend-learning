9015a3dde1b67969cf38bf49b14646e6fedd22a8
jinchenxing <2541226493@qq.com>
2023-04-11 14:01:41 +0800

    登录页面简单实现

---
+++ shop-admin/src/pages/login.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <el-row class="box-border w-screen h-screen">
+            <el-col :span="16" class="bg-blue-400 text-white col-center">
+                <span class="text-5xl font-bold mb-10">管 理 系 统</span>
+                <span class="text-3xl">shop-admin</span>
+            </el-col>
+            <el-col :span="8" class="col-center">
+                <h2 class="text-3xl font-bold">登录</h2>
+                <div class="text-sm text-gray-500 flex w-full justify-center items-center p-5">
+                    <span class="h-[1px] w-1/5 bg-gray-200"></span>
+                    <span class="mx-2 ">账号密码登录</span>
+                    <span class="h-[1px] w-1/5 bg-gray-200"></span>
+                </div>
+                <el-form label-width="120px" label-position="left">
+
+                    <el-form-item label="Username">
+                        <el-input type="text" placeholder="请输入账号" />
+                    </el-form-item>
+
+                    <el-form-item label="Password">
+                        <el-input type="password" placeholder="请输入密码" />
+                    </el-form-item>
+
+                    <el-button type="primary" class="w-80">登 录</el-button>
+                </el-form>
+            </el-col>
+        </el-row>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.col-center {
+    @apply flex flex-col justify-center items-center
+}
+</style>
--- shop-admin/src/router/index.js
+++ shop-admin/src/router/index.js
@@ -3,17 +3,25 @@ import { createRouter, createWebHashHistory } from "vue-router";
 
 import Index from '~/pages/index.vue'
 import About from '~/pages/about.vue'
+import Login from '~/pages/login.vue'
 import NotFound from '~/pages/404.vue'
 
 // 路由数组
 const routes = [
     {
         path: '/',
+        name:"index",
         component: Index
     },
     {
         path: '/about',
+        name:"about",
         component: About
+    },
+    {
+        path: '/login',
+        name:"login",
+        component: Login
     },
     {
         path: '/:pathMatch(.*)*',
